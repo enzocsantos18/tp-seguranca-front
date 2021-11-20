a440f659176abf65f7118fc83bfb07125ecc65ef
Enzo de Carvalho Santos <enzocsantos18@gmail.com>
2021-11-20 10:19:29 -0300

    Navegação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,38 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'two-factor-frontend';
 
+  hideCadastro = true;
+  hideLogin = false;
+  hideLogado = true;
+  hideToken = true;
+
+  ativaCadastrar() {
+    this.hideCadastro = false;
+    this.hideLogin = true;
+    this.hideLogado = true;
+    this.hideToken = true;
+  }
+  ativaToken() {
+    this.hideCadastro = true;
+    this.hideLogin = true;
+    this.hideLogado = true;
+    this.hideToken = false;
+  }
+  ativaLogado() {
+    this.hideCadastro = true;
+    this.hideLogin = true;
+    this.hideLogado = false;
+    this.hideToken = true;
+  }
+
+  ativaLogin() {
+    this.hideCadastro = true;
+    this.hideLogin = false;
+    this.hideLogado = true;
+    this.hideToken = true;
+  }
+
+
   login(usuario: any) {
     console.log(usuario)
 
